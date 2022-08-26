a7eb425d659d1629adf65aadcc98fc47fb030bf9
Buihoang1104 <112138586+Buihoang1104@users.noreply.github.com>
2022-08-26 12:00:23 +0700

    update

---
+++ c1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+
+   int a, b, c, sum;
+   printf ("\n enter eny thrre numbers: ") ;
+   scanf ("%d %d %d", &a,  &b, &c);
+   sum = a + b +c;
+   printf ("\n sum = %d", sum ),
+   return 0;
+   
+   
+   
+}
